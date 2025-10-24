a3e32d24d3e4226c6b5f4b94a65d32dcfac8b7dd
harshvg1 <harshvardhang1@aol.com>
2025-10-25 00:53:20 +0530

    Update deploy_frontend.sh

--- scripts/deploy_frontend.sh
+++ scripts/deploy_frontend.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Copy build files to deployment directory
-mkdir -p /home/ec2-user/medchatbot
-cp -r build/* /home/ec2-user/medchatbot/
-echo "Frontend deployed to /home/ec2-user/medchatbot"
+set -e
+cd /home/ec2-user/frontend || exit
+echo "Installing dependencies..."
+npm install
+npm run build
